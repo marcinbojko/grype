6f10d608a0baf030b647ebc263bf1d19890f4f78
Marcin Bojko <marcin@bojko.com.pl>
2023-03-03 07:55:19 +0100

    Version 0.57.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName        = 'grype'
-$version            = '0.56.0'
+$version            = '0.57.0'
 $url64              = "https://github.com/anchore/grype/releases/download/v"+$version+"/grype_"+$version+"_windows_amd64.zip"
-$checksum64         = '644c2796e16116e544e801f527c6d1c7282244478007e42e2f33926abade7b76'
+$checksum64         = 'fa6159994944854308d300680c6d32b96a39ed84b13aa5c88b4dbea9fc0787de'
 $toolsDir           = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
